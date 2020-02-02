d97990c08f36d70b5d5aa78888e4b86f66db6356
jyuunnii <yuunnii315@gmail.com>
2020-02-02 12:02:39 +0900

    service x controller interacting

--- src/main/java/com/example/test/controller/api/UserApiController.java
+++ src/main/java/com/example/test/controller/api/UserApiController.java
@@ -4,6 +4,8 @@ import com.example.test.ifs.CrudInterface;
 import com.example.test.model.network.Header;
 import com.example.test.model.network.request.UserApiRequest;
 import com.example.test.model.network.response.UserApiResponse;
+import com.example.test.service.UserApiLogicService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -17,10 +19,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("api/user")
 public class UserApiController implements CrudInterface<UserApiRequest, UserApiResponse> {
 
+    @Autowired
+    private UserApiLogicService userApiLogicService;
+
     @Override
     @PostMapping("") //api/user
-    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> userApiRequest) {
-        return null;
+    public Header<UserApiResponse> create(@RequestBody Header<UserApiRequest> request) {
+        return userApiLogicService.create(request);
     }
 
     @Override
