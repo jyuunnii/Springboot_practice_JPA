b32be7ce6d7558b44669c4243b12a8fbad24aec0
jyuunnii <yuunnii315@gmail.com>
2020-02-02 11:16:55 +0900

    CRUD initial

--- src/main/java/com/example/test/controller/api/UserApiController.java
+++ src/main/java/com/example/test/controller/api/UserApiController.java
@@ -2,6 +2,11 @@ package com.example.test.controller.api;
 
 import com.example.test.ifs.CrudInterface;
 import com.example.test.model.network.Header;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -11,22 +16,26 @@ public class UserApiController implements CrudInterface {
 
 
     @Override
+    @PostMapping("") //api/user
     public Header create() {
         return null;
     }
 
     @Override
-    public Header read(Long id) {
+    @GetMapping("{id}") //api/user/{id}
+    public Header read(@PathVariable(name = "id") Long id) {
         return null;
     }
 
     @Override
+    @PutMapping("") //api/user
     public Header update() {
         return null;
     }
 
     @Override
-    public Header delete(Long id) {
+    @DeleteMapping("{id}") //api/user/{id}
+    public Header delete(@PathVariable Long id) {
         return null;
     }
 }
--- src/main/java/com/example/test/model/network/Header.java
+++ src/main/java/com/example/test/model/network/Header.java
@@ -5,7 +5,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
@@ -15,7 +15,7 @@ public class Header<T> {
 
     //api 통신 시간
     @JsonProperty("transaction_time") // camelCase => snake_case 직접 변경
-    private  String transactionTime;
+    private LocalDateTime transactionTime;
 
     //api 응답 코드
     private String resultCode;
@@ -24,6 +24,30 @@ public class Header<T> {
     private String description;
 
     private T data; // body: 응답에서 계속 변하는 부분
+
+    //OK
+    public static <T>Header<T>  OK(){
+        return (Header<T>) Header.builder().transactionTime(LocalDateTime.now())
+                                .resultCode("OK")
+                                .description("OK")
+                                .build();
+    }
+    //Data OK
+    public static <T>Header<T>  OK(T data){
+        return (Header<T>) Header.builder().transactionTime(LocalDateTime.now())
+                .resultCode("OK")
+                .description("OK")
+                .data(data)
+                .build();
+    }
+
+    //Error
+    public static <T>Header<T>  Error(String description){
+        return (Header<T>) Header.builder().transactionTime(LocalDateTime.now())
+                .resultCode("ERROR")
+                .description(description)
+                .build();
+    }
 }
 /*
  header: API 응답에서 공통적으로 들어가는 부분
